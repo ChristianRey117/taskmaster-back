f62b75b189f1aa4f4ae32c229da6491a1be36c95
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-11 16:20:14 -0400

    feat(task-service): add method to post task

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,16 +1,31 @@
 import db from "../database/database";
 import { ITask } from "../models/task";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
 
 class TaskService {
   async getAllTask(): Promise<ITask[]> {
-    const task = await db.query("SELECT * FROM task");
+    const task = await db.query<RowDataPacket[]>("SELECT * FROM task");
     return task as ITask[];
   }
 
   async getProductById(id: number): Promise<ITask | null> {
-    const task = await db.query("SELECT * FROM task WHERE id = ?", id);
+    const task = await db.query<RowDataPacket[]>(
+      "SELECT * FROM task WHERE id = ?",
+      id
+    );
     if (Array.isArray(task) && task.length > 0) {
       return task[0] as ITask;
+    }
+    return null;
+  }
+
+  async postTask(data: ITask): Promise<ITask | null> {
+    const result = await db.query<ResultSetHeader>(
+      `INSERT INTO task SET ?`,
+      data
+    );
+    if (result.insertId) {
+      return await this.getProductById(result.insertId);
     }
     return null;
   }
