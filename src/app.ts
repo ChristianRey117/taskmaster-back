5a6fd5629df0767875b7cde86f7df53ae5e5fae2
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-11 14:24:25 -0400

    feat(app): first petition with connection to database

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import db from "./database/database";
 
 const app = express();
 const PORT = 3000;
+
+app.get("/", async (req: Request, res: Response) => {
+  const result = await db.query("SELECT * FROM task");
+  console.log(result);
+  res.json(result);
+});
 
 app.listen(PORT, () => {
   console.log(`Express is running in port: ${PORT}`);
