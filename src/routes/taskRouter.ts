89f9b2ae8c59c232ae905fedd86788c1f66b4689
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-11 17:00:03 -0400

    feat(task-router): add delete route from task router

--- src/routes/taskRouter.ts
+++ src/routes/taskRouter.ts
@@ -7,5 +7,6 @@ taskRouter.get("/", taskController.getAllTask);
 taskRouter.get("/:id", taskController.getTaskById);
 taskRouter.post("/", taskController.postTask);
 taskRouter.put("/:id", taskController.putTask);
+taskRouter.delete("/:id", taskController.deleteTask);
 
 export default taskRouter;
