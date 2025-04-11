be2175db5a54009da53274e9521754e2178dfc0f
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-11 15:28:43 -0400

    feat(task-controller): create task controller and use task service

---
+++ src/controllers/taskController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import taskService from "../services/taskService";
+import { sendError, sendSuccess } from "../utils/requestHandler";
+
+class TaskController {
+  async getAllTask(req: Request, res: Response) {
+    try {
+      const tasks = await taskService.getAllTask();
+      sendSuccess(res, tasks);
+    } catch (error: any) {
+      sendError(res, error.message);
+    }
+  }
+
+  async getProductById(req: Request, res: Response) {
+    try {
+      const id = Number(req.params["id"]);
+      const task = await taskService.getProductById(id);
+      if (task) {
+        sendSuccess(res, task);
+      } else {
+        sendError(res, "Task not found");
+      }
+    } catch (error: any) {
+      sendError(res, error.message);
+    }
+  }
+}
+
+export default new TaskController();
