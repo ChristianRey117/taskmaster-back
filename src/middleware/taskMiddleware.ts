5a7d791f496c248d522b015c1dd59f85f2850085
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-11 18:35:52 -0400

    feat(task-middleware): add schema from zod to validate data

--- src/middleware/taskMiddleware.ts
+++ src/middleware/taskMiddleware.ts
@@ -1,9 +1,33 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+
+const taskSchema = z.object({
+  id: z.number().optional(),
+  title: z.string().min(3).max(1000),
+  description: z.string().min(3).max(1000),
+  state: z.enum(["TODO", "DOING", "DONE"]),
+});
+
+export type validatedTaskData = z.infer<typeof taskSchema>;
 
 export const taskMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log("in middleware");
+  try {
+    const validatedData = taskSchema.parse(req.body);
+    // Si la validación es exitosa, adjunta los datos validados al objeto de la solicitud
+    (req as any).validatedData = validatedData;
+    next(); // Pasa al siguiente middleware o ruta
+  } catch (error) {
+    // Si la validación falla, el error será una instancia de ZodError
+    if (error instanceof z.ZodError) {
+      res.status(400).json({ errors: error.errors });
+    } else {
+      // Maneja otros tipos de errores si es necesario
+      console.error("Error unexpected:", error);
+      res.status(500).json({ message: "Error server validation." });
+    }
+  }
 };
--- src/routes/taskRouter.ts
+++ src/routes/taskRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import taskController from "../controllers/taskController";
+import { taskMiddleware } from "../middleware/taskMiddleware";
 
 const taskRouter = Router();
 
 taskRouter.get("/", taskController.getAllTask);
 taskRouter.get("/:id", taskController.getTaskById);
-taskRouter.post("/", taskController.postTask);
-taskRouter.put("/:id", taskController.putTask);
+taskRouter.post("/", taskMiddleware, taskController.postTask);
+taskRouter.put("/:id", taskMiddleware, taskController.putTask);
 taskRouter.delete("/:id", taskController.deleteTask);
 
 export default taskRouter;
