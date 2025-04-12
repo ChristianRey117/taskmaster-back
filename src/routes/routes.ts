52c9a22692a7d4fba9e199098079688c9331aa21
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2025-04-12 01:34:41 -0400

    feat(user-routes): add user routes to routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import taskRoutes from "./taskRouter";
+import userRoutes from "./userRouter";
 
 const router = Router();
 
 router.use("/task", taskRoutes);
+router.use("/user", userRoutes);
 
 export default router;
